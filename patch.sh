3c79caea28a795bcdc239c050b89cd2b90896427
Artem Sidorenko <artem@posteo.de>
2019-04-11 10:08:08 +0200

    Lets have own project definition derived from inspec

--- patch.sh
+++ patch.sh
@@ -18,6 +18,8 @@ sed -i '/binaries=/ s/inspec/outspec/g' inspec/omnibus/package-scripts/outspec/p
 sed -i '/echo /s/InSpec/OutSpec/g' inspec/omnibus/package-scripts/outspec/postinst inspec/omnibus/package-scripts/outspec/postrm inspec/omnibus/package-scripts/outspec/preinst
 sed -i '/INSTALLER_DIR=/s/inspec/outspec/g' inspec/omnibus/package-scripts/outspec/postinst
 
-sed -i '/name / s/inspec/outspec/g' inspec/omnibus/config/projects/inspec.rb
-sed -i '/friendly_name / s/InSpec/OutSpec/g' inspec/omnibus/config/projects/inspec.rb
-sed -i "s/maintainer .*/maintainer 'Community <community@example.com>'/g" inspec/omnibus/config/projects/inspec.rb
+cp inspec/omnibus/config/projects/inspec.rb inspec/omnibus/config/projects/outspec.rb
+
+sed -i '/name / s/inspec/outspec/g' inspec/omnibus/config/projects/outspec.rb
+sed -i '/friendly_name / s/InSpec/OutSpec/g' inspec/omnibus/config/projects/outspec.rb
+sed -i "s/maintainer .*/maintainer 'Community <community@example.com>'/g" inspec/omnibus/config/projects/outspec.rb
