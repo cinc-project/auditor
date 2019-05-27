109a1670220b5a457903c2b08cd0a7ce81e5356d
Artem Sidorenko <artem.sidorenko@t-systems.com>
2019-05-27 16:00:37 +0200

    Switch from patching approach to the copy approach
    of own files

---
+++ outspec/omnibus/config/projects/outspec.rb
@@ -0,0 +1,92 @@
+# encoding: utf-8
+#
+# Copyright:: Copyright 2016-2018, Chef Software Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative '../../../lib/inspec/version.rb'
+
+name 'outspec'
+friendly_name 'OutSpec'
+maintainer 'Community <community@example.com>'
+homepage 'https://github.com/inspec/inspec'
+
+license 'Apache-2.0'
+license_file '../LICENSE'
+
+# Defaults to C:/opscode/inspec on Windows
+# and /opt/inspec on all other platforms.
+if windows?
+  install_dir "#{default_root}/opscode/#{name}"
+else
+  install_dir "#{default_root}/#{name}"
+end
+
+build_version Inspec::VERSION
+build_iteration 1
+
+override 'ruby', version: '2.5.3'
+# RubyGems 2.7.0 caused issues in the Jenkins pipelines, trouble installing bundler.
+# This issue is not evident in 2.6.x, hence the pin.
+override 'rubygems', version: '2.6.14'
+
+# grab the current train release from rubygems.org
+train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
+override 'train', version: "v#{train_stable}"
+
+dependency 'preparation'
+
+dependency 'inspec'
+dependency 'outspec'
+
+# Mark all directories world readable.
+dependency 'gem-permissions'
+# Redirect all gem bat files and rb files to point to embedded ruby.
+dependency 'shebang-cleanup'
+# Ensure our SSL cert files are accessible to ruby.
+dependency 'openssl-customization'
+# Remove all .dll.a and .a files needed for static linkage.
+dependency 'clean-static-libs'
+
+dependency 'ruby-cleanup'
+
+package :rpm do
+  signing_passphrase ENV['OMNIBUS_RPM_SIGNING_PASSPHRASE']
+  unless rhel? && platform_version.satisfies?('< 6')
+    compression_level 1
+    compression_type :xz
+  end
+end
+
+package :deb do
+  compression_level 1
+  compression_type :xz
+end
+
+package :pkg do
+  identifier 'com.getchef.pkg.inspec'
+  signing_identity 'Developer ID Installer: Chef Software, Inc. (EU3VF8YLX2)'
+end
+compress :dmg
+
+package :msi do
+  fast_msi true
+  upgrade_code 'DFCD452F-31E5-4236-ACD1-253F4720250B'
+  wix_light_extension 'WixUtilExtension'
+  signing_identity 'E05FF095D07F233B78EB322132BFF0F035E11B5B', machine_store: true
+end
+
+exclude '**/.git'
+exclude '**/bundler/git'
--- patch.sh
+++ patch.sh
@@ -5,37 +5,4 @@ git reset --hard
 git clean -f -d
 cd ..
 
-# patching executable names
-cd inspec/bin
-rename 's/inspec/outspec/' *
-cd ../..
-sed -i '/spec.executables   =/ s/inspec/outspec/g' inspec/inspec.gemspec
-
-# apply all other patches
-# 1. find all patches in the patch directory
-# 2. using the path of patch determine the source file/dir path
-# 3. build the target file/dir path by replacing inspec with outspec in the source path
-# 4. copy the source file/dir to the target file/dir
-# 5. apply the patch
-for p in $(cd patch && find . -type f -name "*.patch" && cd ..); do
-  dir="inspec/$(dirname $p)"
-  src_name=$(basename -s .patch $p)
-  tgt_name=$(echo $src_name | sed 's/inspec/outspec/g')
-  cdir=$(pwd)
-  patch="$cdir/patch/$p"
-
-  cd $dir
-  cp -r $src_name $tgt_name
-
-  if [ -d $tgt_name ]; then
-    cd $tgt_name
-    patch < $patch
-  else
-    patch $tgt_name $patch
-  fi
-
-  cd $cdir
-done
-
-# copy additional files
-cp -rp files/* inspec/
+cp -rp outspec/* inspec/
