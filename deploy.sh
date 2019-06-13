5a7ac7a4cc270ec35feb3f3805df77f5e53e0586
Lance Albertson <lance@osuosl.org>
2019-06-13 10:45:43 -0700

    Re-enable deployment to package cloud

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -e
+FILES_PATH=/data/incoming/outspec/files/
 
-bundle exec package_cloud push cc-build/test/ubuntu/xenial outspec*.deb
-bundle exec package_cloud push cc-build/test/ubuntu/bionic outspec*.deb
+bundle exec package_cloud push cc-build/test/ubuntu/xenial ${FILE_PATH}/ubuntu/16.04/outspec*.deb
+bundle exec package_cloud push cc-build/test/ubuntu/bionic ${FILE_PATH}/ubuntu/18.04/outspec*.deb
+bundle exec package_cloud push cc-build/test/debian/jessie ${FILE_PATH}/debian/8/outspec*.deb
+bundle exec package_cloud push cc-build/test/debian/stretch ${FILE_PATH}/debian/9/outspec*.deb
 
-bundle exec package_cloud push cc-build/test/el/6 outspec*.rpm
-bundle exec package_cloud push cc-build/test/el/7 outspec*.rpm
+bundle exec package_cloud push cc-build/test/el/6 ${FILE_PATH}/centos/6/outspec*.rpm
+bundle exec package_cloud push cc-build/test/el/7 ${FILE_PATH}/centos/7/outspec*.rpm
