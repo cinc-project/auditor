6f59f2ac63f4e5ee997833c687e23f75523779ba
Lance Albertson <lance@osuosl.org>
2019-06-13 12:19:19 -0700

    Fix env var name

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 FILES_PATH=/data/incoming/outspec/files/
 
-bundle exec package_cloud push cc-build/test/ubuntu/xenial ${FILE_PATH}/ubuntu/16.04/outspec*.deb
-bundle exec package_cloud push cc-build/test/ubuntu/bionic ${FILE_PATH}/ubuntu/18.04/outspec*.deb
-bundle exec package_cloud push cc-build/test/debian/jessie ${FILE_PATH}/debian/8/outspec*.deb
-bundle exec package_cloud push cc-build/test/debian/stretch ${FILE_PATH}/debian/9/outspec*.deb
+bundle exec package_cloud push cc-build/test/ubuntu/xenial ${FILES_PATH}/ubuntu/16.04/outspec*.deb
+bundle exec package_cloud push cc-build/test/ubuntu/bionic ${FILES_PATH}/ubuntu/18.04/outspec*.deb
+bundle exec package_cloud push cc-build/test/debian/jessie ${FILES_PATH}/debian/8/outspec*.deb
+bundle exec package_cloud push cc-build/test/debian/stretch ${FILES_PATH}/debian/9/outspec*.deb
 
-bundle exec package_cloud push cc-build/test/el/6 ${FILE_PATH}/centos/6/outspec*.rpm
-bundle exec package_cloud push cc-build/test/el/7 ${FILE_PATH}/centos/7/outspec*.rpm
+bundle exec package_cloud push cc-build/test/el/6 ${FILES_PATH}/centos/6/outspec*.rpm
+bundle exec package_cloud push cc-build/test/el/7 ${FILES_PATH}/centos/7/outspec*.rpm
