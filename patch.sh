ad9a019ccdc05eac695774550a45137a5cfbcd49
Artem Sidorenko <artem.sidorenko@t-systems.com>
2019-09-20 19:54:54 +0200

    First iteration of getting it work without submodules

--- patch.sh
+++ patch.sh
@@ -1,13 +1,16 @@
 #!/bin/sh -e
 
-cd inspec
-git reset --hard
-git clean -f -d
-cd ..
+rm -rf inspec
+
+git clone --depth=10 https://github.com/inspec/inspec.git
+
+#cd inspec
+#git reset --hard
+#git clean -f -d
+#cd ..
 
 cp -rp cinc-auditor/* inspec/
 
 cd inspec/
 patch < ../patch/Gemfile.patch
-
 cd ..
