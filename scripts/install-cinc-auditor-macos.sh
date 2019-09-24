af32e9126af9b52fee2474708f1649fbeb5d2c32
Lance Albertson <lance@osuosl.org>
2019-09-24 10:42:51 -0700

    Update MacOS folder to exclude dash

--- scripts/install-cinc-auditor-macos.sh
+++ scripts/install-cinc-auditor-macos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 hdiutil attach $(find data -name '*.dmg')
-sudo installer -package "$(find "/Volumes/Cinc-Auditor/" -name '*.pkg')" -target /
-hdiutil detach "/Volumes/Cinc-Auditor/"
+sudo installer -package "$(find "/Volumes/Cinc Auditor/" -name '*.pkg')" -target /
+hdiutil detach "/Volumes/Cinc Auditor/"
