8fc035010b49c87515d9e7ac9131fbac8d079a48
Lance Albertson <lance@osuosl.org>
2019-10-02 22:35:59 -0700

    Move to rubygems.sh script to work around CI linting

---
+++ rubygems.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+mkdir -p $HOME/.gem
+touch $HOME/.gem/credentials
+chmod 0600 $HOME/.gem/credentials
+printf -- "---\n:rubygems_api_key: ${GEM_HOST_API_KEY}\n" > $HOME/.gem/credentials
+source /home/omnibus/load-omnibus-toolchain.sh
+cd inspec
+gem build cinc-*.gemspec inspec-bin/cinc-*.gemspec
+gem push cinc-*.gem inspec-bin/cinc-*.gem
+rm -rf $HOME/.gem
