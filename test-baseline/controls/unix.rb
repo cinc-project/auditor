abafa46a1b24eace627d086f678b34df7f6df3c1
Lance Albertson <lance@osuosl.org>
2024-04-22 12:13:29 -0700

    Add Amazon 2023 builds
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test-baseline/controls/unix.rb
+++ test-baseline/controls/unix.rb
@@ -2,8 +2,8 @@ control 'cinc-*nix' do
   impact 1.0
   title 'Validate executables outputs on linux and mac'
   desc 'Outputs should not contain trademarks on linux or mac'
-  only_if { os.family != 'windows' }  
-  
+  only_if { os.family != 'windows' }
+
   describe command('echo test > /tmp/testfile') do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should eq '' }
@@ -24,7 +24,7 @@ control 'cinc-*nix' do
     its(:mode) { should cmp '0755' }
     its(:content) { should include 'Redirecting to cinc-auditor...' }
   end
-  
+
   if os.name == 'mac_os_x'
     describe file('/usr/local/bin/inspec') do
       it { should be_symlink }
